2e8209e7cab5f3b49adcfddb82d3ef440369c695
Setareh Khoylow <59746767+skhoylow8@users.noreply.github.com>
2020-07-24 14:11:20 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react"; //more organized
+import ReactDom from "react-dom";
+// var React = require("react");
+// var ReactDom = require("react-dom");
+
+ReactDom.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
